165194be752b73a74f5eeaa3525d5d51ef61dad8
AlbertoGenoxius <20243rd044@utez.edu.mx>
2025-05-08 11:54:59 -0600

    Hice que ingresar el nombre al usuario

---
+++ Cadena.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Cadena {
+    public static void main(String[] args){
+        Scanner UWU = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        String nombre = UWU.nextLine();
+
+        System.out.println("Tu nombre es: "+ nombre);
+    }
+}
